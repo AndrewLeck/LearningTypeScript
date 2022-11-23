b6d6055f5961ee28349f4700cc887cb75839db12
Andrew Leck <Aleck360@gmail.com>
2022-11-23 10:28:36 -0600

    completed section 1_2

--- Ch01/01_02/src/model.ts
+++ Ch01/01_02/src/model.ts
@@ -1,16 +1,20 @@
-let displayName = "Jess's standing desk";
-let inventoryType = "furniture";
-let trackingNumber = "FD123455";
+let displayName: string = "Jess's standing desk";
+let inventoryType: string = "furniture";
+let trackingNumber: string = "FD123455";
 let createDate = new Date();
 let originalCost = 425;
 
-function getInventoryItem(trackingNumber) {
-
+function getInventoryItem(trackingNumber: string): {
+  displayName: string;
+  inventoryType: string;
+  trackingNumber: string;
+  createDate: Date;
+  origionalCost: Number;
+} {
+  return null;
 }
 
-function saveInventoryItem(item) {
-
-}
+function saveInventoryItem(item) {}
 
 let inventoryItem = getInventoryItem(trackingNumber);
 
